60ca337b311252635d5576123df5da00b54babab
laurene03 <laurenek06@gmail.com>
2025-05-09 16:00:14 +0200

    catalogue

---
+++ www/app/view/catalogue.view.php
@@ -0,0 +1,23 @@
+<main>
+
+    <h1>Nos bières</h1>
+
+    <?php for ($i=0 ; $i<6 ; $i++) : ?>
+        <div class="carte">
+        
+        <img src="/www/public/images/" alt="image de bière">
+        <h2>Minshi</h2>
+        <p>description</p>
+
+    </div>
+
+    <?php endfor ?>
+
+    
+
+
+    
+
+
+
+</main>
