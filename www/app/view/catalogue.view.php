248e92e16160c58e71b1a409df2b48fd574c15dd
laurene03 <laurenek06@gmail.com>
2025-05-12 14:52:33 +0200

    Update catalogue.view.php

--- www/app/view/catalogue.view.php
+++ www/app/view/catalogue.view.php
@@ -3,13 +3,25 @@
     <h1>Nos bières</h1>
 
     <?php for ($i=0 ; $i<6 ; $i++) : ?>
+            
         <div class="carte">
-        
-        <img src="/www/public/images/" alt="image de bière">
-        <h2>Minshi</h2>
-        <p>description</p>
 
-    </div>
+            <a href="#">
+            
+                <img src="public/images/" alt="image de bière">
+
+                <div class="carte-infos">
+
+                    <p class="nom-biere">Minshi</p>
+                    <p class="description">Description</p>
+
+                </div>
+
+            </a>
+
+        </div>
+
+   
 
     <?php endfor ?>
 
